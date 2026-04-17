6d16ab93e4fa73e49889968287a8a57c2bd2dd96
Olga Dementieva <08289@mail.ru>
2026-04-17 22:26:02 +0300

    Добавила обновления для category.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+
+from typing import List
+
 from src.product import Product
 
 
@@ -14,7 +17,25 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """Метод добавления товаров в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Геттер выводит список товаров в виде строк"""
+        product_str = ""
+        for product_ in self.__products:
+            product_str += f"{product_.name}, {product_.price} руб. Остаток: {product_.quantity} шт.\n"
+        return product_str
+
+    @property
+    def products_list(self) -> List[Product]:
+        """Возвращает список продуктов"""
+        return self.__products
