96c2336973c1c2f12d1adbb8bdb5bb082420db72
Olga Dementieva <08289@mail.ru>
2026-04-24 13:27:58 +0300

    Добавила изменения в category.py

--- src/category.py
+++ src/category.py
@@ -31,8 +31,11 @@ class Category:
 
     def add_product(self, product: Product) -> None:
         """Метод добавления товаров в категорию"""
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self) -> str:
