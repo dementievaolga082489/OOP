5df89c4d8d22829c6f327c317873bb3fb48fa518
Olga Dementieva <08289@mail.ru>
2026-04-24 13:32:30 +0300

    Добавила модуль smartphone_product.py

---
+++ src/smartphone_product.py
@@ -0,0 +1,31 @@
+from black.nodes import Union
+
+from src.product import Product
+
+
+class Smartphone(Product):
+    """Дочерний класс 'Смартфон' принимающий родительский класс Product"""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ) -> None:
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+    def __add__(self, other: Union["Smartphone", Product]) -> float:
+        """Сложение стоимости всех товаров 'смартфонов' на складе."""
+        if type(other) is Smartphone:
+            total_cost_products = self.price * self.quantity + other.price * other.quantity
+            return total_cost_products
+        raise TypeError
