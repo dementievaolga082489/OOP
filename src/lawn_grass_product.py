611ac76226dbb16773c14ebf4b755e0029c45d61
Olga Dementieva <08289@mail.ru>
2026-04-24 13:32:08 +0300

    Добавила модуль lawn_grass_product.py

---
+++ src/lawn_grass_product.py
@@ -0,0 +1,29 @@
+from black.nodes import Union
+
+from src.product import Product
+
+
+class LawnGrass(Product):
+    """Дочерний класс 'Трава газонная' принимающий родительский класс Product"""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str,
+    ) -> None:
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __add__(self, other: Union["LawnGrass", Product]) -> float:
+        """Сложение стоимости всех товаров 'газонная трава' на складе."""
+        if type(other) is LawnGrass:
+            total_cost_products = self.price * self.quantity + other.price * other.quantity
+            return total_cost_products
+        raise TypeError
