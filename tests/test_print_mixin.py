3f025e637061046bc14bf083297db4fc15ca464e
Olga Dementieva <08289@mail.ru>
2026-04-27 18:28:04 +0300

    Добавила модуль test_print_mixin.py

---
+++ tests/test_print_mixin.py
@@ -0,0 +1,17 @@
+from src.lawn_grass_product import LawnGrass
+from src.product import Product
+from src.smartphone_product import Smartphone
+
+
+def test_print_mixin(capsys):
+    Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    message = capsys.readouterr()
+    assert message.out.strip() == "Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
+
+    LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    message = capsys.readouterr()
+    assert message.out.strip() == "LawnGrass(Газонная трава, Элитная трава для газона, 500.0, 20)"
+
+    Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    message = capsys.readouterr()
+    assert message.out.strip() == "Smartphone(Iphone 15, 512GB, Gray space, 210000.0, 8)"
